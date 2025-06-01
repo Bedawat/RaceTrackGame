9cdbf8eb3eb44b940a06f32ccd64be610633313c
peter <peter.waetzel@stud.hs-kempten.de>
2025-06-01 14:36:17 +0200

    ProjectStart

---
+++ GameLogic.h
@@ -0,0 +1,14 @@
+//
+// Created by Peter Wätzel on 01.06.25.
+//
+
+#pragma once
+
+
+
+class GameLogic {
+public:
+
+private:
+
+};
---
+++ Track.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Peter Wätzel on 01.06.25.
+//
+
+#include "Track.h"
